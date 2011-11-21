94f5be82214c29501898768d0c08a75021098696
rainy4649 <rainy4649@gmail.com>
2011-11-22 01:39:59 +0900

    commit files
    Signed-off-by: rainy4649 <rainy4649@gmail.com>

---
+++ now_on_sell/config.php
@@ -0,0 +1,18 @@
+<?php
+// DBserver name
+	$db		=	'example.com';
+// User name
+	$user	=	'user';
+// Password
+	$pw		=	'password';
+// EC-site URL
+	$url		=	'http://example.com';
+// DB name
+	$db_name	=	'example.db';
+
+// expression(link to cart)
+	$button		=	'add to cart.';
+// 
+	$finish		=	'finish to sell.';
+
+?>
---
+++ now_on_sell/now_on_sell.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: now_on_sell
+Plugin URI: 
+Description: 【The function to cooperate E-commerce site】<br />If there is any stoks,it appear a button to link E-commerce site.<br />If there is no stock,it display "We have Already finished to sell reserve ticket".
+Version: 1.0
+Author: hakomori
+Author URI: 
+*/
+
+function now_on_sell($pre_id) {
+	//　ID of item
+	$id = $pre_id[id];
+
+	require 'config.php';
+	$conn = mysql_connect($db, $user, $pw) or die(mysql_error());
+
+	// connect to MySQL, select database
+	mysql_query('SET NAMES utf8', $conn);
+	// set character code of query
+	mysql_select_db($db_name) or die(mysql_error());
+
+	// get stock by key as item number
+	$SQL="
+	select dtb_products_class.stock, dtb_products.status
+	 from dtb_products_class, dtb_products
+	 where dtb_products_class.product_id=".$id."
+	 and dtb_products.product_id=".$id.";
+	";
+
+	// excute SQL
+	$res = mysql_query($SQL) or die(mysql_error());
+	$row = mysql_fetch_array($res, MYSQL_NUM);
+	$stock = $row[0];
+	$show= $row[1];
+
+	// free the result, close the connect
+	mysql_free_result($res);
+	mysql_close($conn);
+
+	if($show==1) {
+		if ($stock > 0) {
+			$answer = "<a href='".$url."/products/detail.php?product_id=".$id."' class='online'>". $button ."</a>";
+		}else{
+			$answer = "<span style='color: red;'>". $finish ."</span>";
+		}
+	}else{
+		$answer="";
+	}
+	return $answer;
+}
+
+add_shortcode('now_on_sell', 'now_on_sell');
+
+?>
